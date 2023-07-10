0d1d6d879c2c2e401e55bc9e792ff016dbe74efc
Andre Soares <andregsilv@gmail.com>
2023-07-10 00:48:44 -0300

    Add script to create folders

---
+++ criapastas/criapastas.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"go.uber.org/multierr"
+)
+
+func getWeekDayAbbreviation(weekday time.Weekday) string {
+	weekdayNames := map[time.Weekday]string{
+		time.Sunday:    "Sonntag",
+		time.Monday:    "Montag",
+		time.Tuesday:   "Dienstag",
+		time.Wednesday: "Mittwoch",
+		time.Thursday:  "Donnerstag",
+		time.Friday:    "Freitag",
+		time.Saturday:  "Samstag",
+	}
+
+	result := weekdayNames[weekday]
+	result, _ = strings.CutSuffix(result, "tag")
+	return result
+}
+
+func GetDirNames(date time.Time) []string {
+	dateStr := date.Format("06 01 02")
+	root := fmt.Sprintf("%s %s. Fotos", dateStr, getWeekDayAbbreviation(date.Weekday()))
+	return []string{
+		path.Join(root, fmt.Sprintf("%s Pingos", dateStr)),
+		path.Join(root, fmt.Sprintf("%s Flash Navega", dateStr)),
+
+		path.Join(root, fmt.Sprintf("%s Ecobie", dateStr), "Ecobie 1"),
+		path.Join(root, fmt.Sprintf("%s Ecobie", dateStr), "Ecobie 2"),
+		path.Join(root, fmt.Sprintf("%s Ecobie", dateStr), "Ecobie 3"),
+	}
+}
+
+func main() {
+	dirs := GetDirNames(time.Now())
+
+	var errs error
+	for _, dir := range dirs {
+		fmt.Printf("Creating directory '%s'\n", dir)
+		err := os.MkdirAll(dir, 0o755)
+		multierr.AppendInto(&errs, err)
+		if errs != nil {
+			fmt.Fprintf(os.Stderr, "Failed with error: %s\n", err)
+			return
+		}
+	}
+	fmt.Println("Directories created successfully")
+}
---
+++ criapastas/criapastas_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetDirNames(t *testing.T) {
+	t.Run("Example from video", func(t *testing.T) {
+		date := time.Date(2023, 7, 9, 0, 0, 0, 0, time.Local)
+		dirnames := GetDirNames(date)
+
+		assert.Len(t, dirnames, 5)
+		assert.Equal(t, "23 07 09 Sonn. Fotos/23 07 09 Pingos", dirnames[0])
+		assert.Equal(t, "23 07 09 Sonn. Fotos/23 07 09 Flash Navega", dirnames[1])
+		assert.Equal(t, "23 07 09 Sonn. Fotos/23 07 09 Ecobie/Ecobie 1", dirnames[2])
+		assert.Equal(t, "23 07 09 Sonn. Fotos/23 07 09 Ecobie/Ecobie 2", dirnames[3])
+		assert.Equal(t, "23 07 09 Sonn. Fotos/23 07 09 Ecobie/Ecobie 3", dirnames[4])
+	})
+
+	t.Run("Generated example", func(t *testing.T) {
+		date := time.Date(2007, 6, 5, 0, 0, 0, 0, time.Local)
+		dirnames := GetDirNames(date)
+
+		assert.Len(t, dirnames, 5)
+		assert.Equal(t, "07 06 05 Diens. Fotos/07 06 05 Pingos", dirnames[0])
+		assert.Equal(t, "07 06 05 Diens. Fotos/07 06 05 Flash Navega", dirnames[1])
+		assert.Equal(t, "07 06 05 Diens. Fotos/07 06 05 Ecobie/Ecobie 1", dirnames[2])
+		assert.Equal(t, "07 06 05 Diens. Fotos/07 06 05 Ecobie/Ecobie 2", dirnames[3])
+		assert.Equal(t, "07 06 05 Diens. Fotos/07 06 05 Ecobie/Ecobie 3", dirnames[4])
+	})
+}
