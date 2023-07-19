c765540068e4e61ea5cc1190f94aca004fa6f27a
Andre Soares <andregsilv@gmail.com>
2023-07-19 02:16:20 -0300

    fix tests

--- cmd/createdirs/createdirs_test.go
+++ cmd/createdirs/createdirs_test.go
@@ -12,25 +12,25 @@ func TestDirNamesForSingleDate(t *testing.T) {
 		date := time.Date(2023, 7, 9, 0, 0, 0, 0, time.Local)
 		dirnames := DirNamesForSingleDate(date)
 
-		assert.Len(t, dirnames, 5)
+		assert.Len(t, dirnames, 6)
 		assert.Equal(t, "23 07 09 Sonn. Fotos/23 07 09 Pingos", dirnames[0])
 		assert.Equal(t, "23 07 09 Sonn. Fotos/23 07 09 Flash Navega", dirnames[1])
 		assert.Equal(t, "23 07 09 Sonn. Fotos/23 07 09 Ecobie/Ecobie 1", dirnames[2])
 		assert.Equal(t, "23 07 09 Sonn. Fotos/23 07 09 Ecobie/Ecobie 2", dirnames[3])
 		assert.Equal(t, "23 07 09 Sonn. Fotos/23 07 09 Ecobie/Ecobie 3", dirnames[4])
-		assert.Equal(t, "23 07 09 Sonn. Fotos/23 07 09 Ecobie/Ecobie 4", dirnames[4])
+		assert.Equal(t, "23 07 09 Sonn. Fotos/23 07 09 Ecobie/Ecobie 4", dirnames[5])
 	})
 
 	t.Run("Generated example", func(t *testing.T) {
 		date := time.Date(2007, 6, 5, 0, 0, 0, 0, time.Local)
 		dirnames := DirNamesForSingleDate(date)
 
-		assert.Len(t, dirnames, 5)
+		assert.Len(t, dirnames, 6)
 		assert.Equal(t, "07 06 05 Diens. Fotos/07 06 05 Pingos", dirnames[0])
 		assert.Equal(t, "07 06 05 Diens. Fotos/07 06 05 Flash Navega", dirnames[1])
 		assert.Equal(t, "07 06 05 Diens. Fotos/07 06 05 Ecobie/Ecobie 1", dirnames[2])
 		assert.Equal(t, "07 06 05 Diens. Fotos/07 06 05 Ecobie/Ecobie 2", dirnames[3])
 		assert.Equal(t, "07 06 05 Diens. Fotos/07 06 05 Ecobie/Ecobie 3", dirnames[4])
-		assert.Equal(t, "07 06 05 Diens. Fotos/07 06 05 Ecobie/Ecobie 4", dirnames[4])
+		assert.Equal(t, "07 06 05 Diens. Fotos/07 06 05 Ecobie/Ecobie 4", dirnames[5])
 	})
 }
